d9051bd8828bcfe6a135efe20309d719610896e8
Steven Miller <steven.miller@anz.com>
2015-06-01 17:03:06 +1000

    Make code nicer

--- interceptors/module.js
+++ interceptors/module.js
@@ -1,12 +1,7 @@
 module.exports = function(moduleName, injectorArray) {
+
   this.ANGULAR_MODULE = angular.module(moduleName, injectorArray);
-
-  if (!this.cache[moduleName]) {
-      this.cache[moduleName] = {};
-  } else {
-      this.cache[moduleName] = this.cache[moduleName];
-  }
-
+  this.cache[moduleName] = this.cache[moduleName] || {};
   this.MODULE_CACHE = this.cache[moduleName];
 
   this.name = this.ANGULAR_MODULE.name;
